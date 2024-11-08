14dffb37b72b8a4f4fbcc4741bc25040cdaa5386
andrepradooliveira16 <andrepradooliveira16@gmail.com>
2024-11-08 23:41:56 +0000

    add dev nix

---
+++ .idx/dev.nix
@@ -0,0 +1,33 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    
+  
+ "eamodio.gitlens"
+ "esbenp.prettier-vscode"
+ "GitHub.vscode-pull-request-github"
+ "ms-azuretools.vscode-docker"
+ "Prisma.prisma"
+ "Prisma.prisma-insider"
+ "rangav.vscode-thunder-client"];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
